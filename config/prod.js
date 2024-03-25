7fe2ef701bb8a10f62572c11581f5dd363a3d76c
kirbeee <pspsean1234@gmail.com>
2024-03-25 14:25:41 +0800

    adding prod env

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+// prof.js - Production keys here
+
+module.exports = {
+    googleClientID : process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+}

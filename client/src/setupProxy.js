4e21147a8002a9a75f2ccc046cf86dcd20069107
kirbeee <pspsean1234@gmail.com>
2024-07-12 14:09:26 +0800

    fix frontend proxy issue and remove backend proxy file

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,10 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
     app.use(
-        ["/surveys/*"],
+        ["/api", "/auth/google"],
         createProxyMiddleware({
-            target: "http://localhost:3000",
-            changeOrigin: true
+            target: "http://localhost:5000",
+            changeOrigin:true
         })
     );
 };

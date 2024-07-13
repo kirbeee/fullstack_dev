e55797e3e19bc2fc313c1c7b5af3e6bbe309a03c
kirbeee <pspsean1234@gmail.com>
2024-07-13 13:43:03 +0800

    finish the billing service

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,18 +1,20 @@
-
 const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripSecretKey);
+const requireLogin = require("../middlewares/requireLogin");
 const router = require("express").Router();
 
 router.route("/api/stripe")
-    .post(async (req, res) => {
+    .post(requireLogin,async (req, res) => {
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
             description: '$5 for 5credits',
             source: req.body.id
         });
+        req.user.credits += 5;
+        const user = await req.user.save();
 
-        console.log(charge)
+        res.send(user);
     });
 
 module.exports = router;
